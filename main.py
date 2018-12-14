baec555ab1cf561eb60c78ccfdede3ec6eb81fe7
shreyavshetty <shreyav4@gmail.com>
2018-12-14 21:44:17 +0530

    Added main file

--- main.py
+++ main.py
@@ -5,15 +5,36 @@ from model import restore_model
 import numpy as np
 app = Flask(__name__)
 app.secret_key = 'development key'
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detect.html', methods=['GET', 'POST'])
+def detect():
     form = Form(request.form)
     if request.method == 'POST' : #and form.validate():
         test_values = np.array([[form.pregnancies.data,form.glucose.data,form.bloodPressure.data,form.skinThickness.data,form.insulin.data,form.bmi.data,form.diabetesPedigreeFunction.data,form.age.data],], dtype=np.float32)
         result = restore_model(test_values)
+        print("************")
+        if(result):
+            result = "Time to go Sugar Free"
+        else:
+            result = "Time for dessert"
     else:
         result = None
-    return render_template('home.html', form=form, result=result)
+    iframe = 'details.html'
+
+    return render_template('detect.html', form=form, result=result,iframe=iframe)
+
+@app.route('/details.html')
+def details():
+    return render_template('details.html')
+
+@app.route('/dashboard.html')
+def dashboard():
+    return render_template('dashboard.html')
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
